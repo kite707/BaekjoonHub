1a7f9bed5cad4feb6a33453c365fc11e5467a844
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2023-10-10 18:18:48 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+
+int solution(vector<string> board)
+{
+    int n = board.size(), m = board[0].size();
+    int vis[100][100]; 
+    for(int i=0;i<100;i++){
+        fill(&vis[i][0],&vis[i][0]+100,-1);
+    }
+    queue<pair<int,int>> q;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            if(board[i][j] == 'R') q.push({i,j});
+    vis[q.front().first][q.front().second] = 0;
+    while(!q.empty())
+    {
+        pair<int,int> cur = q.front();
+        q.pop();
+        for(int dir=0;dir<4;dir++)
+        {
+            int nx = cur.first, ny = cur.second;
+            while (1)
+            {
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) break;
+                if (board[nx][ny] == 'D') break;
+                nx += dx[dir];
+                ny += dy[dir];
+            }
+            nx -= dx[dir];
+            ny -= dy[dir];
+            if (board[nx][ny] == 'G') return (vis[cur.first][cur.second] + 1);
+            if (vis[nx][ny]>=0) continue;
+            vis[nx][ny] = vis[cur.first][cur.second] + 1;
+            q.push({nx,ny});
+        }
+    }
+    return (-1);
+}
