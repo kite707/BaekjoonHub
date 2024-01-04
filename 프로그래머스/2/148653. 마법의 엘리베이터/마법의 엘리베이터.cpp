4e52beae137bdd757e3b77ce4211126cac66ec54
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2024-01-04 17:13:36 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -1,40 +1,14 @@
 #include <string>
 #include <vector>
-#include <deque>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
 int solution(int storey) {
-    int answer = 0;
-    deque<int> dq;
-    while(storey!=0){
-        dq.push_back(storey%10);
-        storey/=10;
+    if(storey/10==0){
+       return min(10-storey+1,storey); 
+    }else{
+        return min(10-storey%10+solution(storey/10+1),storey%10+solution(storey/10));
     }
-    int carry = 0;
-    while(!dq.empty()){
-        int curNumber = dq.front();
-        curNumber+=carry;
-        dq.pop_front();
-        cout<<"cur is "<<curNumber<<endl;
-        if(curNumber>5){
-            answer+=(10-curNumber);
-            carry = 1;
-            cout<<answer<<endl;
-            if(dq.size()==0){
-                answer++;
-            }
-        }else if(curNumber==5 && dq.front()>=5){
-            answer+=(10-curNumber);
-            carry = 1;
-            cout<<answer<<endl;
-        }
-        else{
-            answer+=curNumber;
-            carry = 0;
-            cout<<answer<<endl;
-        }
-    }
-    return answer;
 }
