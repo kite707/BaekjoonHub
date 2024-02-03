7d3417b66331de917cd3e5c346436c3ba8f8be58
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2024-02-03 17:55:46 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 68 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,33 +1,27 @@
 #include <iostream>
-#include <list>
+#include <queue>
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-   int N,K;
-   cin>>N>>K;
-   list<int> li;
-    auto cursor = li.end();
+    queue<int> q;
+    int N,K;
+    cin>>N>>K;
     for(int i=1;i<=N;i++){
-       li.insert(cursor,i);
-   }
-    cursor = li.begin();
-   cout<<"<";
-   while(li.size()!=1){
-       for(int i=1;i<K;i++){
-           if(cursor==--li.end()){
-               cursor=li.begin();
-           }
-           else{cursor++;}
-       }
-       cout<<*cursor<<", ";
-       cursor=li.erase(cursor);
-       if(cursor==li.end()){
-           cursor = li.begin();
-       }
-   }
-   cout<<*li.begin()<<">";
-
+        q.push(i);
+    }
+    K--;
+    cout<<"<";
+    while(q.size()!=1){
+        for(int i=0;i<K;i++){
+            int front = q.front();
+            q.pop();
+            q.push(front);
+        }
+        cout<<q.front()<<", ";
+        q.pop();
+    }
+    cout<<q.front()<<">";
 }
