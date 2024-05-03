4e751e24f5e0e83625279dfb62d8b8ac24d560f8
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2024-05-03 17:23:57 +0900

    [level 2] Title: 후보키, Time: 0.27 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.cpp
+++ 프로그래머스/2/42890. 후보키/후보키.cpp
@@ -1,64 +1,31 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
-
-bool isAns(vector<int> a, vector<int> b){
-        for(int j=0;j<a.size();j++){
-            if(a[j]==1&&b[j]!=1){
-                return true;
-            }
-        }
-    return false;
+bool possi(vector<int> &vec,int now){
+    for(int i=0;i<vec.size();i++)
+        if((vec[i]&now)==vec[i])return false;
+    return true;
 }
-
 int solution(vector<vector<string>> relation) {
-    int answer = 0;
-    int totalColumn=relation[0].size();
-    int totalRow=relation.size();
-    vector<vector<int>> ans;
-    for(int i=1;i<=totalColumn;i++){
-        vector<int> btn(totalColumn,1);
-        for(int j=0;j<totalColumn-i;j++){
-            btn[j]=0;
+    int n=relation.size();
+    int m=relation[0].size();
+    //cout<<"n와 m은 "<<n<<" "<<m<<endl;
+    vector<int> ans;
+    for(int i=1;i<(1<<m);i++){
+        if(ans.size()!=0&&!possi(ans,i))continue;
+        set<string> s;
+        for(int j=0;j<n;j++){
+            string now="";
+            for(int k=0;k<m;k++){
+                
+                if(i&(1<<k)){
+                //     cout<<"i는 "<<bitset<4>(i)<<endl;
+                // cout<<"1<<k는 "<<bitset<4>(1<<k)<<endl;
+                    now+=relation[j][k];
+                }
+            }
+            s.insert(now);
         }
-        do{
-            if(ans.size()!=0){
-                bool flag=true;
-                for(int t=0;t<ans.size();t++){
-                    if(!isAns(ans[t],btn)){
-                        flag=false;
-                        break;
-                    }
-                }
-                if(!flag)continue;
-            }
-            unordered_map<string,int> mp;
-            bool isAnswer=true;
-            for(auto rel:relation){
-                string key="";
-                for(int idx=0;idx<btn.size();idx++){
-                    if(btn[idx]==1)key+=rel[idx];
-                }
-                mp[key]++;
-                if(mp[key]!=1){
-                    isAnswer=false;
-                    break;
-                }
-            }
-            if(isAnswer){
-            //        cout<<"후보키 추가\n";
-            // for(auto k:btn){
-            //     cout<<k<<" ";
-            // }cout<<endl;
-            ans.push_back(btn);
-            answer++; 
-            
-                } 
-        }while(next_permutation(btn.begin(),btn.end()));
-        
+        if(s.size()==n)ans.push_back(i);
     }
-    return answer;
+    return ans.size();
 }
