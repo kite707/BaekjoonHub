1030f59b59bececcccf03cc02f7bb06d60bbd83c
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2024-08-22 23:04:20 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 5.80 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -1,50 +1,43 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <queue>
-#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int dx[4]={0,1,0,-1};
-int dy[4]={-1,0,1,0};
 int fosil[501];
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
 
-int bfs(int y, int x,vector<vector<int>> &land){
-    int maxX=x;
-    int minX=x;
+void bfs(int y, int x, vector<vector<int>> &land){
     land[y][x]=0;
-    int cnt=1;
+    int ans=1;
     queue<pair<int,int>> q;
     q.push({y,x});
+    int maxX=x;
+    int minX=x;
     while(!q.empty()){
         int curX=q.front().second;
         int curY=q.front().first;
+        q.pop();
         maxX=max(maxX,curX);
         minX=min(minX,curX);
-        q.pop();
         for(int i=0;i<4;i++){
             int nxtX=curX+dx[i];
             int nxtY=curY+dy[i];
             if(nxtX<0||nxtY<0||nxtX>=land[0].size()||nxtY>=land.size())continue;
-            if(land[nxtY][nxtX]==1){
-                q.push({nxtY,nxtX});
-                land[nxtY][nxtX]=0;
-                cnt++;
-            }
+            if(land[nxtY][nxtX]==0)continue;
+            ans++;
+            land[nxtY][nxtX]=0;
+            q.push({nxtY,nxtX});
         }
     }
-    //cout<<"min max의 값: "<<minX<<" "<<maxX<<endl;
     for(int i=minX;i<=maxX;i++){
-        fosil[i]+=cnt;
+        fosil[i]+=ans;
     }
-    return cnt;
 }
 
 int solution(vector<vector<int>> land) {
     int answer = 0;
-    int width=land[0].size();
-    priority_queue<int> q;
-    q.push(0);
     for(int i=0;i<land.size();i++){
         for(int j=0;j<land[0].size();j++){
             if(land[i][j]==1){
@@ -52,10 +45,5 @@ int solution(vector<vector<int>> land) {
             }
         }
     }
-    // cout<<endl;
-    // for(int i=0;i<land[0].size();i++){
-    //     cout<<fosil[i]<<" ";
-    // }
-    
     return *max_element(&fosil[0],&fosil[0]+land[0].size());
 }
