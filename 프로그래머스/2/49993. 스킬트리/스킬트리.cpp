7d2cba0fa05e225b553106fd8db0c9b80326af2a
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2024-08-28 18:37:12 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -1,40 +1,28 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
-
-bool isExist[27][2];
-
-bool compareSkill(string skillTree, string userSkill){
-    for(int i=0;i<userSkill.length();i++){
-        if(userSkill[i]!=skillTree[i])return false;
-    }
-    return true;
-}
 
 int solution(string skill, vector<string> skill_trees) {
     int answer = 0;
-    for(auto k:skill){
-        isExist[k-'A'][0]=true;
+    unordered_map<char,int> mp;
+    for(int i=0;i<skill.size();i++){
+        mp.insert({skill[i],i});
     }
     for(int i=0;i<skill_trees.size();i++){
         string curSkill=skill_trees[i];
-        string newSkill="";
-        for(auto k:curSkill){
-            if(isExist[k-'A'][0]&&!isExist[k-'A'][1]){
-                newSkill+=k;
-                isExist[k-'A'][1]=true;
+        int curMax=0;
+        for(int j=0;j<curSkill.size();j++){
+            if(mp.find(curSkill[j])!=mp.end()){
+            int val=mp[curSkill[j]];
+            if(val!=curMax)break;
+            curMax++;
             }
-        }
-        for(int i=0;i<27;i++){
-            isExist[i][1]=false;
-        }
-        skill_trees[i]=newSkill;
-    }
-    for(int i=0;i<skill_trees.size();i++){
-        if(compareSkill(skill,skill_trees[i])){
-            answer++;
+            if(j==curSkill.size()-1){answer++;}
         }
     }
+
     return answer;
 }
