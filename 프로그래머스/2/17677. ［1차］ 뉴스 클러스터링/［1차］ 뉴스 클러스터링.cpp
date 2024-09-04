b2568702eca522fdcceae0d3b3ed4489aa1e34de
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2024-09-04 19:32:09 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,60 +1,29 @@
 #include <string>
+#include <algorithm>
 #include <iostream>
-#include <unordered_map>
 using namespace std;
 
-bool isValid(char a){
-    if(a<'A')return false;
-    if(a>'Z')return false;
-    return true;
-}
+int arr1[705];
+int arr2[705];
 
-unordered_map<string,int> tokenize(string str){
-    unordered_map<string,int> mp;
-    for(int i=0;i<str.size()-1;i++){
-        char cur=str[i];
-        char nxt=str[i+1];
-        if(!isValid(cur)||!isValid(nxt))continue;
-        string s;
-        s.push_back(cur);
-        s.push_back(nxt);
-        mp[s]++;
-    }
-    return mp;
-
-}
+float sizeA, sizeB, sizeSame;
 
 int solution(string str1, string str2) {
-    int answer = 65536;
-    for(int i=0;i<str1.size();i++){
-        str1[i]=toupper(str1[i]);
-    }
-    for(int i=0;i<str2.size();i++){
-        str2[i]=toupper(str2[i]);
-    }
-    unordered_map<string,int> mp1=tokenize(str1);
-    unordered_map<string,int> mp2=tokenize(str2);
-    int sameSize=0;
-    
-    int mp1Size=0;
-    int mp2Size=0;
-    for(auto &k:mp1){
-        mp1Size+=mp1[k.first];
-    }
-    
-    for(auto &k:mp2){
-        string key=k.first;
-        mp2Size+=mp2[key];
-        if(mp1[key]!=0){
-            sameSize+=min(mp1[key],mp2[key]);
+    int answer = 0;
+    for(int i=0;i<str1.size()-1;i++){
+        if(isalpha(str1[i])&&isalpha(str1[i+1])){
+            arr1[(str1[i]&31)*26+(str1[i+1]&31)]++;
+            sizeA++;
         }
     }
-    int add=mp1Size+mp2Size-sameSize;
-    
-    if(add==0){
-        return answer;
+    for(int i=0;i<str2.size()-1;i++){
+        if(isalpha(str2[i])&&isalpha(str2[i+1])){
+            arr2[(str2[i]&31)*26+(str2[i+1]&31)]++;
+            sizeB++;
+        }
     }
-    
-    float ans=(float)sameSize/(float)add;
-    return answer*ans;
+    for(int i=0;i<705;i++){
+        if(arr1[i]!=0&&arr2[i]!=0)sizeSame+=min(arr1[i],arr2[i]);
+    }
+    return sizeA+sizeB==0?65536:(sizeSame/(sizeA+sizeB-sizeSame))*65536;
 }
