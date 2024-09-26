76055431e276e83597bdf3f2a75112f4867eb274
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2024-09-26 21:58:32 +0900

    [level 2] Title: 후보키, Time: 0.89 ms, Memory: 3.62 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.cpp
+++ 프로그래머스/2/42890. 후보키/후보키.cpp
@@ -1,31 +1,31 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <set>
 using namespace std;
-bool possi(vector<int> &vec,int now){
-    for(int i=0;i<vec.size();i++)
-        if((vec[i]&now)==vec[i])return false;
+
+vector<int> ans;
+bool possible(int n){
+    for(int an:ans)if((n&an)==an)return false;
     return true;
 }
+
 int solution(vector<vector<string>> relation) {
-    int n=relation.size();
-    int m=relation[0].size();
-    //cout<<"n와 m은 "<<n<<" "<<m<<endl;
-    vector<int> ans;
-    for(int i=1;i<(1<<m);i++){
-        if(ans.size()!=0&&!possi(ans,i))continue;
+    int colu=relation.size();
+    int attr=relation[0].size();
+    for(int i=1;i<(1<<attr);i++){
+        if(!possible(i))continue;
         set<string> s;
-        for(int j=0;j<n;j++){
-            string now="";
-            for(int k=0;k<m;k++){
-                
-                if(i&(1<<k)){
-                //     cout<<"i는 "<<bitset<4>(i)<<endl;
-                // cout<<"1<<k는 "<<bitset<4>(1<<k)<<endl;
-                    now+=relation[j][k];
+        for(int k=0;k<relation.size();k++){
+            string cand;
+            for(int j=0;j<attr;j++){
+                if(i>>j&1){
+                    cand+=relation[k][j];
                 }
             }
-            s.insert(now);
+            s.insert(cand);
         }
-        if(s.size()==n)ans.push_back(i);
+       if(s.size()==colu)ans.push_back(i);
     }
     return ans.size();
 }
