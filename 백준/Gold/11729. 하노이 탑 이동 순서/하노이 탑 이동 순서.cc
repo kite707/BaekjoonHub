529f01a1327f7b08d435e371d35b4da63f7ed911
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2024-10-12 16:45:14 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,24 +1,39 @@
+//3개를 3번막대로 옮기기 = 7번
+//4개를 3번막대로 옮기기 = 3개를 2번막대로 옮기기 + 4를 3으로, 나머지 3개 옮기기
+//n개를 st번에서 en번 막대로 옮기기
+//n-1개를 6-st-en으로 옮기기+가장 큰 조각 옮기기 + n-1개를 6-st-en에서 en으로 옮기기
+
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-void solution(int a, int b, int n){//a에서 b로 n개의 기둥 옮기기
+long long arr[21];
+
+long long getTotal(int N){
+    if(arr[N]!=0)return arr[N];
+    arr[N]=getTotal(N-1)*2+1;
+    return arr[N];
+}
+
+void move(int n, int st, int en){
+    int remain=6-st-en;
     if(n==1){
-        cout<<a<<" "<<b<<"\n";
+        cout<<st<<" "<<en<<"\n";
         return;
     }
-    solution(a,6-a-b,n-1);
-    cout<<a<<" "<<b<<"\n";
-    solution(6-a-b,b,n-1);
+    move(n-1,st,remain);
+    cout<<st<<" "<<en<<"\n";
+    move(n-1,remain,en);
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
-    int n;
-    cin>>n;
-    cout<<(1<<n)-1<<"\n";
-    solution(1,3,n);
+    
+    int N;
+    cin>>N;
+    arr[1]=1;
+    arr[2]=3;
+    cout<<getTotal(N)<<"\n";
+    move(N,1,3);
 }
