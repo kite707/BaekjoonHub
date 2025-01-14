53e77a8d3b929c08623706ac5648d388850a075f
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2025-01-14 21:41:09 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 5.81 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -2,48 +2,57 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
-int fosil[501];
 int dx[4]={0,1,0,-1};
 int dy[4]={1,0,-1,0};
 
-void bfs(int y, int x, vector<vector<int>> &land){
+int arr[500];
+
+void bfs(vector<vector<int>> &land, int y, int x){
+    int total=1;
     land[y][x]=0;
-    int ans=1;
     queue<pair<int,int>> q;
-    q.push({y,x});
-    int maxX=x;
+
     int minX=x;
+    int maxX=x;
+
+    q.push({y,x});
     while(!q.empty()){
-        int curX=q.front().second;
         int curY=q.front().first;
+        int curX=q.front().second;
+
         q.pop();
-        maxX=max(maxX,curX);
-        minX=min(minX,curX);
         for(int i=0;i<4;i++){
-            int nxtX=curX+dx[i];
-            int nxtY=curY+dy[i];
-            if(nxtX<0||nxtY<0||nxtX>=land[0].size()||nxtY>=land.size())continue;
-            if(land[nxtY][nxtX]==0)continue;
-            ans++;
-            land[nxtY][nxtX]=0;
-            q.push({nxtY,nxtX});
+        int nxtY=curY+dy[i];
+        int nxtX=curX+dx[i];
+
+        if(nxtY<0||nxtX<0||nxtY>=land.size()||nxtX>=land[i].size()) continue;
+        if(land[nxtY][nxtX]==0) continue;
+        land[nxtY][nxtX]=0;
+        total++;
+        q.push({nxtY,nxtX});
+        minX=min(nxtX,minX);
+        maxX=max(nxtX,maxX);
         }
     }
     for(int i=minX;i<=maxX;i++){
-        fosil[i]+=ans;
+        arr[i]+=total;
     }
 }
 
+
 int solution(vector<vector<int>> land) {
     int answer = 0;
+    priority_queue<int> q;
+   
     for(int i=0;i<land.size();i++){
         for(int j=0;j<land[0].size();j++){
             if(land[i][j]==1){
-                bfs(i,j,land);
+                bfs(land,i,j);
             }
         }
     }
-    return *max_element(&fosil[0],&fosil[0]+land[0].size());
+    return *max_element(&arr[0],&arr[0]+500);
 }
