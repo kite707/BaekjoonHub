d3f73ae5490f754c5a6315e54a0794518f2da2af
HyeYeon Kang <70741257+kite707@users.noreply.github.com>
2025-01-16 20:30:08 +0900

    [level 2] Title: 요격 시스템, Time: 199.87 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/README.md
+++ 프로그래머스/2/181188. 요격 시스템/README.md
@@ -1,10 +1,10 @@
 # [level 2] 요격 시스템 - 181188 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188) 
 
 ### 성능 요약
 
-메모리: 94.6 MB, 시간: 804.34 ms
+메모리: 96.8 MB, 시간: 199.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 08:38:16
+2025년 01월 16일 20:30:05
 
 ### 문제 설명
 
--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -4,28 +4,21 @@
 #include <iostream>
 using namespace std;
 
-bool cmp(vector<int> a, vector<int> b){
-    if(a[1]!=b[1])return a[1]>b[1];
-    return a[0]<b[0];
-}
-
 int solution(vector<vector<int>> targets) {
-    int answer = 0;
-    sort(targets.begin(),targets.end(),cmp);
+    int answer = 1;
+    sort(targets.begin(),targets.end());
     int curMin=targets[0][0];
-    
-    for(int i=0;i<targets.size();i++){
-        
-        if(curMin>=targets[i][1]){
+    int curMax=targets[0][1];
+    for(auto k:targets){
+        if(k[0]>=curMax){
+            curMin=k[0];
+            curMax=k[1];
             answer++;
-            curMin=targets[i][0];
-            //cout<<"curMin is "<<curMin<<endl;
+            continue;
         }
-        curMin=max(targets[i][0],curMin);
-        //cout<<targets[i][0]<<" "<<targets[i][1]<<endl;
-        
-        
+        curMin=max(curMin,k[0]);
+        curMax=min(curMax,k[1]);
     }
     
-    return ++answer;
+    return answer;
 }
